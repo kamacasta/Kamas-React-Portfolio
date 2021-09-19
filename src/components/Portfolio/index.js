f4b55a958a188461a6635621efca594cd50b256c
kama casta <kamacasta983@gmail.com>
2021-09-19 09:51:02 -0700

    added jsx to portfolio jsx and worked on its css will comeback toit later

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,6 +3,36 @@ import React from 'react';
 function Portfolio () {
     return(
         <section className="my-5">
+            <h1 id="portfolio"><em>Portfolio</em></h1>
+            <p> A selection of my work </p>
+        <div className="portfolio">
+            
+        <a href="" className="portfolio-item">
+                <img src="" className="portfolio-img"/>
+            </a>
+
+        <a href="" className="portfolio-item">
+                <img src="" className="portfolio-img"/>
+        </a>
+
+        <a href="" className="portfolio-item">
+                <img src="" className="portfolio-img"/>
+            </a>
+
+        <a href="" className="portfolio-item">
+                <img src="" className="portfolio-img"/>
+        </a>
+
+        <a href="" className="portfolio-item">
+                <img src="" className="portfolio-img"/>
+            </a>
+
+        <a href="" className="portfolio-item">
+                <img src="" className="portfolio-img"/>
+        </a>
+        
+        
+        </div>
 
         </section>
     );
