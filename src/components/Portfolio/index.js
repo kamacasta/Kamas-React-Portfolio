b1a72416c2af56a72b802ef881bf59234200a603
kama casta <kamacasta983@gmail.com>
2021-09-20 18:35:58 -0700

    for the most part the portfolio component is completed will comeback to it later to make minor changes, this is not yet completed

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,7 +4,7 @@ import portfolioImage2 from '../../assets/cover/Mustache-Mustangs.png';
 import portfolioImage3 from '../../assets/cover/Password-Generator.png';
 import portfolioImage4 from '../../assets/cover/Weather-Dashboard.png';
 import portfolioImage5 from '../../assets/cover/Work-Day-Scheduler.png';
-// import portfolioImage6from '../../assets/cover/';
+// import portfolioImage6 from '../../assets/cover/';
 
 function Portfolio () {
     return(
@@ -14,27 +14,27 @@ function Portfolio () {
         <div className="portfolio" style={{ width: "50% "}}>
             
         <a href="https://kamacasta.github.io/Fuzzy-Friend-Finder/" target="_blank" className="portfolio-item">
-                <img src={portfolioImage1} className="portfolio-img" alt=""/>
+                <img src={portfolioImage1} className="portfolio-img" alt="One of 6 images of kamas portfolio images"/>
             </a>
 
         <a href="https://mustache-mustangs.herokuapp.com/"  target="_blank" className="portfolio-item">
-                <img src={portfolioImage2} className="portfolio-img"/>
+                <img src={portfolioImage2} className="portfolio-img" alt="Two of 6 images of kamas portfolio images"/>
         </a>
 
         <a href="https://kamacasta.github.io/Friendly-Parakeet_PW/" target="_blank" className="portfolio-item">
-                <img src={portfolioImage3} className="portfolio-img"/>
+                <img src={portfolioImage3} className="portfolio-img" alt="Three of 6 images of kamas portfolio images"/>
             </a>
 
         <a href="https://kamacasta.github.io/Weather-Dashboard/"  target="_blank" className="portfolio-item">
-                <img src={portfolioImage4} className="portfolio-img"/>
+                <img src={portfolioImage4} className="portfolio-img" alt="Four of 6 images of kamas portfolio images"/>
         </a>
 
         <a href="https://kamacasta.github.io/Work-Day-Scheduler/"  target="_blank" className="portfolio-item">
-                <img src={portfolioImage5} className="portfolio-img"/>
+                <img src={portfolioImage5} className="portfolio-img" alt="Five of 6 images of kamas portfolio images"/>
             </a>
 
         {/* <a href="" className="portfolio-item">
-                <img src={portfolioImage6} className="portfolio-img"/>
+                <img src={portfolioImage6} className="portfolio-img" alt="Six of 6 images of kamas portfolio images"/>
         </a>
          */}
         
