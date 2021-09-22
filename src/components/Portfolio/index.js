27b4aeae351d3ace9d5c8e51388bc5c33241e752
kama casta <kamacasta983@gmail.com>
2021-09-22 11:33:57 -0700

    added rels to my portfolio links

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -14,27 +14,28 @@ function Portfolio () {
         <div className="portfolio" style={{ width: "50% "}}>
             
         <a href="https://kamacasta.github.io/Fuzzy-Friend-Finder/" target="_blank" className="portfolio-item">
-                <img src={portfolioImage1} className="portfolio-img" alt="One of 6 images of kamas portfolio images"/>
+                <img src={portfolioImage1} className="portfolio-img" alt="One of 6 images of kamas portfolio images"
+                rel="noreferrer"/>
             </a>
 
         <a href="https://mustache-mustangs.herokuapp.com/"  target="_blank" className="portfolio-item">
-                <img src={portfolioImage2} className="portfolio-img" alt="Two of 6 images of kamas portfolio images"/>
+                <img src={portfolioImage2} className="portfolio-img" alt="Two of 6 images of kamas portfolio images" rel="noreferrer"/>
         </a>
 
         <a href="https://kamacasta.github.io/Friendly-Parakeet_PW/" target="_blank" className="portfolio-item">
-                <img src={portfolioImage3} className="portfolio-img" alt="Three of 6 images of kamas portfolio images"/>
+                <img src={portfolioImage3} className="portfolio-img" alt="Three of 6 images of kamas portfolio images" rel="noreferrer"/>
             </a>
 
         <a href="https://kamacasta.github.io/Weather-Dashboard/"  target="_blank" className="portfolio-item">
-                <img src={portfolioImage4} className="portfolio-img" alt="Four of 6 images of kamas portfolio images"/>
+                <img src={portfolioImage4} className="portfolio-img" alt="Four of 6 images of kamas portfolio images" rel="noreferrer"/>
         </a>
 
         <a href="https://kamacasta.github.io/Work-Day-Scheduler/"  target="_blank" className="portfolio-item">
-                <img src={portfolioImage5} className="portfolio-img" alt="Five of 6 images of kamas portfolio images"/>
+                <img src={portfolioImage5} className="portfolio-img" alt="Five of 6 images of kamas portfolio images" rel="noreferrer"/>
             </a>
 
         <a href="https://gist.github.com/kamacasta" className="portfolio-item">
-                <img src={portfolioImage6} className="portfolio-img" alt="Six of 6 images of kamas portfolio images"/>
+                <img src={portfolioImage6} className="portfolio-img" alt="Six of 6 images of kamas portfolio images" rel="noreferrer"/>
         </a>
         
         </div>
